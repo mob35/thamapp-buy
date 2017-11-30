8fdb9dfbf8e702ff19cbb4d493512a551eca2a21
mob35 <toonelicious@gmail.com>
2017-11-30 13:03:25 +0700

    update

--- src/components/ion-form-shipping/ion-form-shipping.ts
+++ src/components/ion-form-shipping/ion-form-shipping.ts
@@ -19,7 +19,19 @@ export class IonFormShippingComponent {
 
   address = {};
   deli = 0;
+  total = 0;
   data: any = {
+    order: {
+      shipping: {},
+      items: [],
+      payment: {},
+      amount: 0,
+      discount: 0,
+      totalamount: 0,
+      deliveryprice: 0
+    }
+  };
+  dataOrd: any = {
     order: {
       shipping: {},
       items: [],
@@ -33,7 +45,6 @@ export class IonFormShippingComponent {
   constructor(
     private dialogs: Dialogs
   ) {
-    console.log('Hello IonFormShippingComponent Component');
   }
   selectaddress(data) {
     this.data.order.shipping = data;
@@ -48,17 +59,24 @@ export class IonFormShippingComponent {
       detail: ship.shippingtype.detail,
       price: ship.shippingprice
     }
-    console.log(ship);
-    console.log(shipping);
+
     var checkProduct = false;
     if (this.data.order.items && this.data.order.items.length > 0) {
-      // console.log('+++++++++++++++++++++++++++++++++');
       this.data.order.items.forEach(itm => {
         if (itm.product.name === product.product.name) {
           itm.delivery = shipping;
           checkProduct = true;
         }
       });
+      // console.log(this.data.order);
+      this.deli = 0;
+      this.total = 0;
+      this.data.order.items.forEach(itm => {
+        this.deli += itm.delivery.price || 0;
+      });
+      this.total = (this.listshipping.totalamount) + this.deli || 0;
+      // console.log(this.deli);
+
     }
     if (!checkProduct) {
       this.data.order.items.push({
@@ -69,35 +87,39 @@ export class IonFormShippingComponent {
         price: product.product.price,
         discount: product.discount,
         afterdiscount: (product.amount || 0) - (product.discount || 0)
-
       });
-      if (this.data.order.items.length === this.listshipping.items.length) {
-        this.data.order.items.forEach(itm => {
-          this.deli += itm.delivery.price || 0;
-          this.listshipping.totalamount = (this.listshipping.totalamount) += itm.delivery.price || 0;
-        });
-      }
+      this.deli = 0;
+      this.total = 0;
+      this.data.order.items.forEach(itm => {
+        this.deli += itm.delivery.price || 0;
+      });
+      this.total = (this.listshipping.totalamount) + this.deli || 0;
     }
-    console.log(this.data.order);
   }
   stepValidation() {
+    this.data.order.deliveryprice = 0;
+    this.data.order.discount = 0;
+    this.data.order.amount = 0;
+    this.data.order.totalamount = 0;
     if (this.data.order.shipping && this.data.order.shipping.address) {
       if (this.data.order.items.length === this.listshipping.items.length) {
         this.data.order.items.forEach(itm => {
-          console.log(itm);
           this.data.order.deliveryprice += itm.delivery.price || 0;
           this.data.order.discount += itm.discount || 0;
           this.data.order.amount += itm.amount || 0;
           this.data.order.totalamount += itm.afterdiscount || 0;
         });
-        console.log(this.data.order);
-        this.gotoNext.emit(this.data);
+        this.dataOrd.order = this.data.order;
+        this.gotonextPage(this.dataOrd);
       } else {
         this.dialogs.alert('Please select products delivery', 'Delivery');
       }
     } else {
       this.dialogs.alert('Please select shipping', 'Shipping');
     }
+  }
+  gotonextPage(data) {
+    this.gotoNext.emit(data);
   }
 
 }
